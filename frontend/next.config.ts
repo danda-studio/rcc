e2313f773f50a04b4891447d4f5e7d21f16d9418
Nagiev Vyusal <111697286+Nagiev100@users.noreply.github.com>
2025-10-11 22:18:45 +0300

    Init project Next,typeScript,tailwind,turbopack,eslint,shadcnUI

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    experimental: {
-        turbo: {
-            rules: {},
-        },
-    },
+    /* config options here */
 };
 
 export default nextConfig;

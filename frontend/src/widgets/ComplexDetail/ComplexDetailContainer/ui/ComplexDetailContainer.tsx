5a78917be5c67c448119cf455f63595a5922b0c5
vyusal <vyusalb@inbox.ru>
2025-10-13 18:08:22 +0300

    Add in project ComplexApartmentCatalog,     ComplexDetailContactForm,     ComplexDetailBankOffers,     ComplexDetailFeatures,     ComplexDetailHero,     ComplexDetailIntroBlock,     ComplexDetailTimeline,     ComplexDetailNavigationPanel

--- frontend/src/widgets/ComplexDetail/ComplexDetailContainer/ui/ComplexDetailContainer.tsx
+++ frontend/src/widgets/ComplexDetail/ComplexDetailContainer/ui/ComplexDetailContainer.tsx
@@ -1,7 +1,31 @@
+"use client"
+import { useSelector } from 'react-redux';
+import {RootState} from "@/shared/redux/store";
+import {
+    ComplexApartmentCatalog,
+    ComplexDetailContactForm,
+    ComplexDetailBankOffers,
+    ComplexDetailFeatures,
+    ComplexDetailHero,
+    ComplexDetailIntroBlock,
+    ComplexDetailTimeline,
+    ComplexDetailNavigationPanel
+} from "@/widgets/ComplexDetail";
+
 export const ComplexDetailContainer = () => {
+    const complex = useSelector((state: RootState) => state.complex.list);
+
     return (
-        <>
-        <h2>DetailPage</h2>
-        </>
-    )
+        <section>
+            <h2>DetailPage</h2>
+            <ComplexDetailHero/>
+            <ComplexDetailFeatures/>
+            <ComplexDetailIntroBlock/>
+            <ComplexDetailBankOffers/>
+            <ComplexDetailTimeline/>
+            <ComplexApartmentCatalog/>
+            <ComplexDetailContactForm/>
+            <ComplexDetailNavigationPanel/>
+        </section>
+    );
 }

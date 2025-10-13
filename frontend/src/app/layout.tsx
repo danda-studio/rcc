30616317e2e8ba37ef457009dc501b878c835a72
vyusal <vyusalb@inbox.ru>
2025-10-13 18:08:49 +0300

    Add in project ComplexApartmentCatalog,     ComplexDetailContactForm,     ComplexDetailBankOffers,     ComplexDetailFeatures,     ComplexDetailHero,     ComplexDetailIntroBlock,     ComplexDetailTimeline,     ComplexDetailNavigationPanel

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import "../shared/styles/globals.css";
 import {ReduxProvider} from "@/shared/redux/provider/ReduxProvider";
 
 export const metadata: Metadata = {
---
+++ frontend/src/widgets/ComplexDetail/ComplexDetailBankOffers/ui/ComplexDetailBankOffers.tsx
@@ -0,0 +1,5 @@
+export const ComplexDetailBankOffers = () => {
+    return (
+        <></>
+    )
+}

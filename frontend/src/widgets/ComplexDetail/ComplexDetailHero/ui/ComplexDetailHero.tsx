ecc01c8b8a3af004f593b746998621f1c8d857ad
Nagiev Vyusal <111697286+Nagiev100@users.noreply.github.com>
2025-10-14 21:46:01 +0300

    Block Hero

--- frontend/src/widgets/ComplexDetail/ComplexDetailHero/ui/ComplexDetailHero.tsx
+++ frontend/src/widgets/ComplexDetail/ComplexDetailHero/ui/ComplexDetailHero.tsx
@@ -9,16 +9,28 @@ export const ComplexDetailHero = () => {
             className="rounded-2xl w-full h-[62.5rem] relative overflow-hidden
             bg-[linear-gradient(180deg,#345783_0%,#192435_27%,#101828_54%,#03050C_79%,#01050C_100%)]"
         >
-
-            <article className="flex items-center justify-between px-[2rem] mt-[2.5rem]">
-                <div className="flex items-center border border-white rounded-2xl ">
-                    <Image src={Flag} alt={"Flag"} className="w-[2.4rem] h-[2.4rem]" />
-                    <p className="font-manrope font-medium text-[2rem] leading-[100%] text-[rgba(255,255,255,1)]">
+            <article className="flex items-center justify-between px-[2rem] mt-[2.5rem] mb-4">
+                <div
+                    className="flex items-center border border-white rounded-2xl px-[1rem] py-[0.4rem] gap-[0.5rem]"
+                      style={{
+                          background: 'linear-gradient(180deg, rgba(255, 255, 255, 0.10) 100%, rgba(255, 255, 255, 0) 0%)',
+                      }}
+                >
+                    <Image src={Flag} alt={"Flag"} className="w-[1.5rem] h-[1.5rem]"/>
+                    <p className="font-manrope font-medium text-[1.2rem] leading-[100%] text-[rgba(255,255,255,1)]">
                         Мариуполь
                     </p>
                 </div>
-                <Image src={Logo} alt={"Logo"}/>
+                <Image src={Logo} alt={"Logo"} className="translate-y-4"/>
             </article>
+            <section className="flex px-[2rem] relative">
+                <div className="absolute z-10 flex flex-col items-left justify-center max-w-[40rem]">
+                    <h2 className="font-manrope font-medium text-[7.4rem] leading-[92%] tracking-[-0.04em] mb-7">
+                        Продажи в новом ЖК "Олимпийский"
+                    </h2>
+                    <p className="text-[rgba(255,255,255,1)] font-manrope font-medium text-[2.1rem] tracking-[-0.04em]">Место, где хочется жить</p>
+                </div>
+            </section>
             <article className="absolute right-[-27rem] bottom-0">
                 <Image src={Complex} alt="Complex" className="object-cover"/>
             </article>

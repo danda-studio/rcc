1cd84f8ec58541a19ff068f8fa231cca12e7277c
vyusal <vyusalb@inbox.ru>
2025-10-15 23:50:10 +0300

    Refactor styles

--- frontend/src/widgets/ComplexDetail/ComplexDetailHero/ui/HeroHeader.tsx
+++ frontend/src/widgets/ComplexDetail/ComplexDetailHero/ui/HeroHeader.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 export const HeroHeader = () => {
     return (
-        <article className="flex items-center justify-between px-10 mt-12">
+        <article className="flex items-center justify-between px-10 mt-12 mb-4">
             <div
                 className="flex items-center border border-white rounded-2xl px-4 py-2 gap-2 bg-radial-hero"
             >
--- frontend/src/widgets/ComplexDetail/ComplexDetailHero/ui/HeroMainContent.tsx
+++ frontend/src/widgets/ComplexDetail/ComplexDetailHero/ui/HeroMainContent.tsx
@@ -6,7 +6,7 @@ import Ceil from "../../../../shared/images/png/ceil.png"
 export const HeroMainContent = () => {
     return (
         <section className="flex px-10">
-            <div className="flex flex-col items-left justify-center max-w-222 gap-6">
+            <div className="flex flex-col items-left max-w-222">
                 <h2 className="font-manrope font-medium text-header tracking-[-0.04em] bg-test-text">
                     Продажи в новом ЖК "Олимпийский"
                 </h2>
@@ -16,10 +16,10 @@ export const HeroMainContent = () => {
             </div>
             <div className="flex relative z-10 gap-41.5">
                 <div className="flex flex-col gap-59">
-                    <Image src={Armor} alt={"Armor"} className="w-23 h-23 translate-x-37 translate-y-25"/>
-                    <Image src={Procent} alt={"Procent"} className="w-47.75 h-42 translate-x-40 translate-y-25"/>
+                    <Image src={Armor} alt={"Armor"} className="w-23 h-23 translate-x-37 translate-y-22"/>
+                    <Image src={Procent} alt={"Procent"} className="w-47.75 h-42 translate-x-40 translate-y-22"/>
                 </div>
-                <Image src={Ceil} alt={"Ceil"} className="w-78.5 h-42 translate-x-40 translate-y-45"/>
+                <Image src={Ceil} alt={"Ceil"} className="w-78.5 h-42 translate-x-40 translate-y-42"/>
             </div>
         </section>
     )

a1bf369428595f3915a1e8538c6343231be010ae
Nagiev Vyusal <111697286+Nagiev100@users.noreply.github.com>
2025-10-15 15:40:23 +0300

    Block Hero styles

--- frontend/src/widgets/ComplexDetail/ComplexDetailHero/ui/ComplexDetailHero.tsx
+++ frontend/src/widgets/ComplexDetail/ComplexDetailHero/ui/ComplexDetailHero.tsx
@@ -8,31 +8,29 @@ export const ComplexDetailHero = () => {
         <section
             className="rounded-2xl h-250 relative bg-radial-1 overflow-hidden"
         >
-            <article className="flex items-center justify-between px-10 mt-10 mb-4">
+            <article className="flex items-center justify-between px-10 mt-12 mb-5">
                 <div
-                    className="flex items-center border border-white rounded-2xl px-[1rem] py-[0.4rem] gap-[0.5rem]"
-                    style={{
-                        background: 'linear-gradient(180deg, rgba(255, 255, 255, 0.10) 100%, rgba(255, 255, 255, 0) 0%)',
-                    }}
+                    className="flex items-center border border-white rounded-2xl px-4 py-2 gap-2 bg-radial-hero"
                 >
-                    <Image src={Flag} alt={"Flag"} className="w-[1.5rem] h-[1.5rem]"/>
+                    <Image src={Flag} alt={"Flag"} className="w-6 h-6"/>
                     <p className="font-manrope font-medium text-[1.2rem] leading-[100%] text-[rgba(255,255,255,1)]">
                         Мариуполь
                     </p>
                 </div>
-                <Image src={Logo} alt={"Logo"} className="translate-y-4 w-11.5 h-18"/>
+                <Image src={Logo} alt={"Logo"} className="translate-y-3 w-11.5 h-18"/>
             </article>
             <section className="flex px-10 relative">
-                <div className="absolute z-10 flex flex-col items-left justify-center max-w-[40rem]">
-                    <h2 className="font-manrope font-medium text-header tracking-[-0.04em] mb-7 bg-test-text">
-                        Продажи в новом ЖК "Олимпийский"
+                <div className="absolute z-10 flex flex-col items-left justify-center max-w-222 gap-6">
+                    <h2 className="font-manrope font-medium text-header tracking-[-0.04em] bg-test-text">
+                        Продажи в новом ЖК "Олимпийский"
                     </h2>
-                    <p className="text-[rgba(255,255,255,1)] font-manrope font-medium text-[2.1rem] tracking-[-0.04em]">Место,
-                        где хочется жить</p>
+                    <p className="font-manrope font-medium text-subtitle-hero tracking-[-0.04em] text-a">
+                        Место, где хочется жить
+                    </p>
                 </div>
             </section>
-            <article className="absolute right-[-27rem] bottom-0">
-                <Image src={Complex} alt="Complex" className="object-cover w-480 h-270"/>
+            <article className="absolute right-[-26.5rem] bottom-0">
+                <Image src={Complex} alt="Complex" className="object-cover w-480 h-250"/>
             </article>
         </section>
 

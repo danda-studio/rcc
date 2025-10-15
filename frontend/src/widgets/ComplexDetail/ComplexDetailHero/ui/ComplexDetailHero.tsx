109d6b2ff0c031cb50e4091cb1d388d618f2f7b4
vyusal <vyusalb@inbox.ru>
2025-10-16 01:23:08 +0300

    Refactor styles

--- frontend/src/widgets/ComplexDetail/ComplexDetailHero/ui/ComplexDetailHero.tsx
+++ frontend/src/widgets/ComplexDetail/ComplexDetailHero/ui/ComplexDetailHero.tsx
@@ -11,11 +11,9 @@ export const ComplexDetailHero = () => {
         >
             <HeroHeader/>
             <HeroMainContent/>
-            {/* <article className="absolute md:-right-[26.5rem] bottom-0"> */}
             <Image src={Complex} alt="Complex" className="absolute max-md:left-1/2 max-md:-translate-x-1/2 bottom-0 md:-right-[26.5rem] w-30 md:w-480 md:h-250"/>
-            {/* </article> */}
-
-            <Button size="lg" >aDD</Button>
+            <Button >aDD</Button>
+            <div className=" h-20 blur-[50px] backdrop-blur-[100px] bg-[rgba(3,5,12,0.09)]"></div>
         </section>
 
     );

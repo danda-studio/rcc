3fcf65d312ec58e0250c6cb3b6e09ed3628c0bef
vyusal <vyusalb@inbox.ru>
2025-10-15 17:13:01 +0300

    refactor code

--- frontend/src/widgets/ComplexDetail/ComplexDetailHero/ui/ContainerBlure.tsx
+++ frontend/src/widgets/ComplexDetail/ComplexDetailHero/ui/ContainerBlure.tsx
@@ -7,7 +7,7 @@ export const ContainerBlure = (props: ContainerBlureType) => {
     const {title, subTitle} = props;
 
     return (
-        <div className="flex items-center border border-white rounded-2xl px-4 py-2 gap-2 bg-radial-hero">
+        <div className="flex bg-radial-hero">
             <p>{title}</p>
             <p>{subTitle}</p>
         </div>
--- frontend/src/widgets/ComplexDetail/ComplexDetailHero/ui/HeroMainContent.tsx
+++ frontend/src/widgets/ComplexDetail/ComplexDetailHero/ui/HeroMainContent.tsx
@@ -4,17 +4,17 @@ import Armor from "../../../../shared/images/svg/armor.svg"
 
 export const HeroMainContent = () => {
     return (
-        <section className="flex px-10 relative z-10">
-            <div className="absolute z-10 flex flex-col items-left justify-center max-w-222 gap-6">
+        <section className="flex px-10">
+            <div className="flex flex-col items-left justify-center max-w-222 gap-6">
                 <h2 className="font-manrope font-medium text-header tracking-[-0.04em] bg-test-text">
                     Продажи в новом ЖК "Олимпийский"
                 </h2>
-                <p className="font-manrope font-medium text-subtitle-hero tracking-[-0.04em] text-a">
+                <p className="font-manrope font-medium text-subtitle-hero tracking-[-0.04em] text-subtitle">
                     Место, где хочется жить
                 </p>
             </div>
-            <div className="flex">
-                <Image src={Armor} alt={"Armor"}/>
+            <div className="flex relative z-10">
+                <Image src={Armor} alt={"Armor"} className="w-23 h-23"/>
                 <ContainerBlure title={"от 11500р"} subTitle={"Платеж по ипотеке"}/>
                 <ContainerBlure title={"от 2%"} subTitle={"Ипотека"}/>
             </div>

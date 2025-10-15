0ef4689be641d4e8af101315cf7e2a134379537a
Nikita Nedelko <nikita.nedelko.job@gmail.com>
2025-10-16 01:00:53 +0300

    Tailwind tests

--- frontend/src/widgets/ComplexDetail/ComplexDetailHero/ui/ComplexDetailHero.tsx
+++ frontend/src/widgets/ComplexDetail/ComplexDetailHero/ui/ComplexDetailHero.tsx
@@ -11,9 +11,9 @@ export const ComplexDetailHero = () => {
         >
             <HeroHeader/>
             <HeroMainContent/>
-            <article className="absolute right-[-26.5rem] bottom-0">
-                <Image src={Complex} alt="Complex" className="object-cover w-480 h-250"/>
-            </article>
+            {/* <article className="absolute md:-right-[26.5rem] bottom-0"> */}
+            <Image src={Complex} alt="Complex" className="absolute max-md:left-1/2 max-md:-translate-x-1/2 bottom-0 md:-right-[26.5rem] w-30 md:w-480 md:h-250"/>
+            {/* </article> */}
 
             <Button size="lg" >aDD</Button>
         </section>

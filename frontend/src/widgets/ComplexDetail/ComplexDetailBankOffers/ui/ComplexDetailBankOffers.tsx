8fc2b6fd5ce29245bdfb9ce16fef133bbe3b97fb
Nikita Nedelko <nikita.nedelko.job@gmail.com>
2025-10-18 00:38:44 +0300

    DkaLAMzb: Del old

--- frontend/src/widgets/ComplexDetail/ComplexDetailBankOffers/ui/ComplexDetailBankOffers.tsx
+++
@@ -1,5 +0,0 @@
-export const ComplexDetailBankOffers = () => {
-    return (
-        <></>
-    )
-}
--- frontend/src/widgets/ComplexDetail/ComplexDetailFeatures/ui/ComplexDetailFeatures.tsx
+++
@@ -1,5 +0,0 @@
-export const ComplexDetailFeatures = () => {
-    return (
-        <></>
-    )
-}

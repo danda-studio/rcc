542b663597406463c3e7412ffe51dc06a94db43b
danda <we@danda.studio>
2025-10-19 00:57:15 +0300

    DkaLAMzb: Главный блок (#3)
    * DkaLAMzb: Fix banner
    
    * DkaLAMzb: Add readable-tailwind and linting
    
    * DkaLAMzb: Fix Glass
    
    ---------
    
    Co-authored-by: Nikita Nedelko <nikita.nedelko.job@gmail.com>

--- frontend/src/shared/ui/glass/UIGlass.tsx
+++ frontend/src/shared/ui/glass/UIGlass.tsx
@@ -4,7 +4,7 @@ import { cx } from "class-variance-authority";
 
 const borderVariants: Record<NonNullable<UIGlassProps["border"]>, string> = {
   corner: "before:absolute before:inset-0 -before:z-10 before:border-[calc(2.33/16*1rem)] before:rounded-[inherit] before:[clip-path:var(--clip-path-2)]",
-  default: "before:absolute before:inset-0 -before:z-10 before:bg-radial-(--radial-2) before:[clip-path:var(--clip-path-1)]",
+  default: "before:absolute before:inset-0 -before:z-10 before:bg-radial-(--radial-2) before:[clip-path:var(--clip-path-3)] md:before:[clip-path:var(--clip-path-1)]",
 };
 
 export const UIGlass: FC<UIGlassProps> = ({
@@ -14,7 +14,14 @@ export const UIGlass: FC<UIGlassProps> = ({
 }) => {
   return (
     <div className={cx(
-      "bg-white/9 shadow-1 backdrop-blur-md rounded-md overflow-hidden",
+      `
+        bg-white/9
+        shadow-1
+        backdrop-blur-md
+        rounded-sm
+        md:rounded-md
+        overflow-hidden
+      `,
       borderVariants[border],
       className,
     )}

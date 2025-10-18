fdcd0c8c287aceb587f2e3def318b17d49201d42
Nikita Nedelko <nikita.nedelko.job@gmail.com>
2025-10-19 00:55:45 +0300

    DkaLAMzb: Fix Glass

--- frontend/src/shared/ui/glass/UIGlass.tsx
+++ frontend/src/shared/ui/glass/UIGlass.tsx
@@ -4,7 +4,7 @@ import { cx } from "class-variance-authority";
 
 const borderVariants: Record<NonNullable<UIGlassProps["border"]>, string> = {
   corner: "before:absolute before:inset-0 -before:z-10 before:border-[calc(2.33/16*1rem)] before:rounded-[inherit] before:[clip-path:var(--clip-path-2)]",
-  default: "before:absolute before:inset-0 -before:z-10 before:bg-radial-(--radial-2) before:[clip-path:var(--clip-path-1)]",
+  default: "before:absolute before:inset-0 -before:z-10 before:bg-radial-(--radial-2) before:[clip-path:var(--clip-path-3)] md:before:[clip-path:var(--clip-path-1)]",
 };
 
 export const UIGlass: FC<UIGlassProps> = ({
@@ -18,7 +18,8 @@ export const UIGlass: FC<UIGlassProps> = ({
         bg-white/9
         shadow-1
         backdrop-blur-md
-        rounded-md
+        rounded-sm
+        md:rounded-md
         overflow-hidden
       `,
       borderVariants[border],

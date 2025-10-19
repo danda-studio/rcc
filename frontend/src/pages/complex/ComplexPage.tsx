3d024ba5874961660ce2056c9a73d27bd1fec603
Nikita Nedelko <nikita.nedelko.job@gmail.com>
2025-10-19 16:49:38 +0300

    xTvQh72m: Create widget structure

--- frontend/src/pages/complex/ComplexPage.tsx
+++ frontend/src/pages/complex/ComplexPage.tsx
@@ -3,11 +3,14 @@ import { ComplexBanner } from "@/widgets/complex";
 import { ComplexCatalogWidget, ComplexConstructionWidget, ComplexFooterWidget, ComplexMortgageWidget, ResidentialWidget } from "@/widgets/complex/ui";
 
 import { ComplexAboutWidget } from "@/widgets/complex/ui/about";
+import { ContactFormWidget } from "@/widgets/contact";
 
 export const ComplexPage: FC = () => {
   return (
     <>
       <ComplexCatalogWidget />
+      <ContactFormWidget />
+
       <ComplexBanner />
       <ComplexAboutWidget />
 
---
+++ frontend/src/widgets/contact/ui/form/ContactFormWidget.tsx
@@ -0,0 +1,78 @@
+import type { FC } from "react";
+import Image from "next/image";
+
+export const ContactFormWidget: FC = () => {
+  return (
+    <section className={`
+      relative
+      overflow-hidden
+      bg-gray-1
+      border-gray-7
+      rounded-md
+      h-208
+      md:h-193.75
+    `}
+    >
+      <Image
+        width={1920}
+        height={1080}
+        alt="Hero image"
+        src="/images/complex/banner.png"
+        className={`
+          absolute
+          -left-51.75
+          md:left-95.5
+          -bottom-22.5
+          md:-bottom-30
+          min-w-191.5
+          md:w-480
+          min-h-107.75
+          md:h-270
+        `}
+      />
+
+      <div className={`
+        absolute
+        inset-0
+        px-4
+        md:px-10
+        py-8
+        md:py-15
+      `}
+      >
+        <div className={`
+          w-80
+          md:w-120.5
+        `}
+        >
+          <h2 className={`
+            font-medium
+            text-blue-6
+            text-lg-x
+            md:text-4-5xl
+            mb-7
+            md:mb-4
+          `}
+          >
+            Готовы к
+            <br />
+            новоселью?
+          </h2>
+
+          <p className={`
+            text-gray-4
+            font-medium
+            leading-[1.2]
+            text-base
+            md:text-md-x
+          `}
+          >
+            Оставьте заявку — и наш специалист поможет выбрать квартиру в
+            <span className="text-blue-6"> ЖК «Олимпийский» </span>
+            под ваши цели и бюджет
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};

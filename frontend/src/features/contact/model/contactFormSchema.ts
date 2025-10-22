2a2f66674dfb8d4f081160850e08aca45af1ddd0
Nikita Nedelko <nikita.nedelko.job@gmail.com>
2025-10-23 02:09:19 +0300

    Fix form

--- frontend/src/features/contact/model/contactFormSchema.ts
+++ frontend/src/features/contact/model/contactFormSchema.ts
@@ -6,7 +6,10 @@ export const contactFormSchema = z.object({
   name: z.string().min(2, "Введите имя"),
   phone: z.object({
     code: z.string().min(1, "Выберите код страны"),
-    number: z.string().regex(/^\d+$/, "Номер должен содержать только цифры").min(2, "Введите номер телефона"),
+    number: z.string()
+      .regex(/^\d+$/, "Номер должен содержать только цифры")
+      .min(5, "Минимальная длина номера 5 символов")
+      .max(15, "Максимальная длина номера 15 символов"),
   }),
   contactMethod: z.enum(ContactMethod),
   email: z.email("Некорректный email"),

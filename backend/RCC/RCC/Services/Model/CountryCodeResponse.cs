45c543b7389751306d884bdfc0ff0458014efd8d
downh <downh@DESKTOP-AR9ARAH>
2025-10-23 01:47:02 +0300

    добавление дто для метода получения кодов

---
+++ backend/RCC/RCC/Services/Model/CountryCodeResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace RCC.Services.Model
+{
+    public class CountryCodeResponse
+    {
+        public string Region { get; set; } = string.Empty;
+        public string CountryPhoneCode { get; set; } = string.Empty;
+        public string CountryName { get; set;} = string.Empty;
+        public string Mask { get; set; } = string.Empty;
+    }
+}

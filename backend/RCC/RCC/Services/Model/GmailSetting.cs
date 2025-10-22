d74ca7264d601e40332a85ba6ef2b31f261de04a
downh <downh@DESKTOP-AR9ARAH>
2025-10-22 23:57:13 +0300

    1

--- backend/RCC/RCC/Services/Model/GmailSetting.cs
+++ backend/RCC/RCC/Services/Model/GmailSetting.cs
@@ -4,7 +4,6 @@
     {
         public string SmtpServer { get; set; } = string.Empty;
         public int SmtpPort { get; set; }
-        public string ApiKey { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
         public List<string> Recipients { get; set;} = new List<string>();

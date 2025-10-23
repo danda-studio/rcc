80eca4474d8c3388ade4f0894e6c125eb9873e61
out_semen <Semenenko.S.A@citilink.ru>
2025-10-23 14:26:14 +0300

    вернул валидацию

--- frontend/src/features/contact/model/contactFormSchema.ts
+++ frontend/src/features/contact/model/contactFormSchema.ts
@@ -2,16 +2,16 @@ import { z } from "zod";
 import { ContactMethod } from "@/shared/api/scheme";
 
 export const contactFormSchema = z.object({
-  name: z.string().min(2),
+  name: z.string().min(2, "Введите имя"),
   phone: z.object({
-    code: z.string().min(1),
+    code: z.string().min(1, "Выберите код страны"),
     number: z.string()
-      .regex(/^\d+$/)
-      .min(5)
-      .max(15),
+      .regex(/^\d+$/, "Номер должен содержать только цифры")
+      .min(5, "Минимальная длина номера 5 символов")
+      .max(15, "Максимальная длина номера 15 символов"),
   }),
   contactMethod: z.enum(ContactMethod),
-  email: z.email(),
+  email: z.email("Некорректный email"),
 });
 
 export type ContactFormValues = z.infer<typeof contactFormSchema>;

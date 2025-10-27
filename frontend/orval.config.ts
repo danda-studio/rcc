a5726636fb1d41cd317fdd56d8ca03ae3e9cba0a
rainhard23 <moiredmi7@gmail.com>
2025-10-27 23:11:57 +0300

    поправлен выбор кода страны

--- frontend/orval.config.ts
+++ frontend/orval.config.ts
@@ -5,7 +5,7 @@ module.exports = {
       target: "./src/shared/api/generated.ts",
       schemas: "./src/shared/api/scheme",
       client: "react-query",
-      baseUrl: "https://diplomatic-staci-rcc-c14bdd3f.koyeb.app",
+      baseUrl: "http://api.rsk-olimpiyskiy.ru",
     },
   },
 };

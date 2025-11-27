44bcd69da34c9afca91023da40d26f3a7bb747cf
Nagiev Vyusal <111697286+Nagiev100@users.noreply.github.com>
2025-11-27 23:28:06 +0200

    Rcc f m1 xy5 cx/add yandex metrick (#62)
    * rcc/ add Yandex metric for project
    
    * rcc/ refactor code
    
    * rcc/ refactor code

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Providers, UILayout } from "@/shared";
 import { manropeFont } from "@/shared/styles/fonts/manrope";
 import "@/shared/styles/globals.css";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://your-domain.com"),
@@ -45,6 +46,33 @@ export default function RootLayout({
           href="/images/complex/banner.webp"
           type="image/webp"
           fetchPriority="high"
+        />
+
+        {/* Yandex.Metrika */}
+        <Script
+            id="yandex-metrika"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+              (function(m,e,t,r,i,k,a){
+                m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {
+                  if (document.scripts[j].src === r) { return; }
+                }
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+              })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=105547532', 'ym');
+
+              ym(105547532, 'init', {
+                ssr:true,
+                webvisor:true,
+                clickmap:true,
+                ecommerce:"dataLayer",
+                accurateTrackBounce:true,
+                trackLinks:true
+              });
+            `,
+            }}
         />
       </head>
       <body>

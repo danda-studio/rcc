f42ec875f2c0d99581ec3c907c22f0311b6fea24
vyusal <vyusalb@inbox.ru>
2025-11-27 23:41:07 +0200

    rcc/ lintfix

--- frontend/src/shared/lib/analytics/yandexMetrika.ts
+++ frontend/src/shared/lib/analytics/yandexMetrika.ts
@@ -7,8 +7,6 @@ interface YMEventData {
 export const reachGoal = (goalName: string, data?: YMEventData) => {
   if (typeof ym === "function") {
     ym(105547532, "reachGoal", goalName, data);
-    console.log(`Цель '${goalName}' отправлена в Яндекс.Метрику`, data ?? {});
   } else {
-    console.warn("Yandex Metrika не инициализирована");
   }
 };

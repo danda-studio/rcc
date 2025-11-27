f7203f8316a29d16291cf52ab3a86df0f1e845fa
vyusal <vyusalb@inbox.ru>
2025-11-27 23:11:09 +0200

    rcc/ add Yandex metric for project

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -46,6 +46,33 @@ export default function RootLayout({
           type="image/webp"
           fetchPriority="high"
         />
+
+        {/* Yandex.Metrika */}
+        <script
+            id="yandex-metrika"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+              (function(m,e,t,r,i,k,a){
+                m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {
+                  if (document.scripts[j].src === r) { return; }
+                }
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+              })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=105547532', 'ym');
+
+              ym(105547532, 'init', {
+                ssr:true,
+                webvisor:true,
+                clickmap:true,
+                ecommerce:"dataLayer",
+                accurateTrackBounce:true,
+                trackLinks:true
+              });
+            `,
+            }}
+        />
       </head>
       <body>
         <Providers>

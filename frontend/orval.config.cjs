f06a69826c3077f9c58cce3c38d22690a57a64a0
Nikita Nedelko <nikita.nedelko.job@gmail.com>
2025-12-09 05:08:54 +0300

    bItxG4Tn: Fix yaml

--- frontend/orval.config.cjs
+++ frontend/orval.config.cjs
@@ -2,7 +2,7 @@ console.log("API_URL", process.env.API_URL);
 
 module.exports = {
   client: {
-    input: `${process.env.API_URL}/swagger/v1/swagger.json`,
+    input: `${process.env.SCHEMA_URL}/swagger/v1/swagger.json`,
     output: {
       target: "./src/shared/api/generated.ts",
       schemas: "./src/shared/api/scheme",

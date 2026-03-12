2b78f5e49b7c96b7ef4bed423d4d86696c6b258c
Nagiev Vyusal <111697286+Nagiev100@users.noreply.github.com>
2026-03-12 23:09:45 +0200

    Refactor code

--- frontend/src/features/contact/ui/form/ContactFormFeature.tsx
+++ frontend/src/features/contact/ui/form/ContactFormFeature.tsx
@@ -17,33 +17,13 @@ import { Button } from "@/shared/lib/shadcn/ui/button";
 import { Field } from "@/shared/lib/shadcn/ui/field";
 import { cn } from "@/shared/lib/shadcn/utils";
 import { UIPhoneField } from "@/shared/ui/phone-field/UIPhoneField";
+import {getTrackingParams} from "@/features/contact/ui/form/helpers/getTrackingParams";
 
 interface SubmitButton { variant: "default" | "danger"; label: string }
 
 const DEFAULT_BUTTON: SubmitButton = {
   label: "Подобрать квартиру",
   variant: "default",
-};
-
-const getTrackingParams = () => {
-  const params = new URLSearchParams(window.location.search);
-
-  const utm = {
-    utmSource: params.get("utm_source"),
-    utmMedium: params.get("utm_medium"),
-    utmCampaign: params.get("utm_campaign"),
-    utmContent: params.get("utm_content"),
-    utmTerm: params.get("utm_term"),
-    gclid: params.get("gclid"),
-    fbclid: params.get("fbclid"),
-    yclid: params.get("yclid"),
-    ttclid: params.get("ttclid"),
-    clickId: params.get("click_id"),
-  };
-
-  return Object.fromEntries(
-      Object.entries(utm).filter(([, value]) => value !== null)
-  );
 };
 
 export const ContactFormFeature: FC<{ className?: string; button?: SubmitButton }> = ({
